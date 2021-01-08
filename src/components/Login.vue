e86e58b9f8682408e03fedda657c8fb395504887
钟鑫 <490336534@qq.com>
2021-01-09 07:51:21 +0800

    配置axios 发起登录请求

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -64,8 +64,11 @@
         this.$refs.loginFormRef.resetFields();
       },
       login() {
-        this.$refs.loginFormRef.validate((valid) => {
-          console.log(valid);
+        this.$refs.loginFormRef.validate(async valid => {
+          if (!valid) return;
+          const {data: res} = await this.$http.post("login", this.loginForm)
+          if (res.meta.status !== 200) return console.log('登录失败')
+          console.log('登录成功')
         })
       }
     }
--- src/main.js
+++ src/main.js
@@ -6,6 +6,11 @@ import './plugins/element.js'
 import './assets/fonts/iconfont.css'
 // 导入全局样式表
 import './assets/css/global.css'
+
+import axios from "axios";
+// 配置请求的根路径
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
+Vue.prototype.$http = axios
 Vue.config.productionTip = false
 
 new Vue({
