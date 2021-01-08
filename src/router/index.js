02f2f65ddcf94c1502441ae252e99c53c95c1c80
钟鑫 <490336534@qq.com>
2021-01-09 07:59:54 +0800

    新建Home页面

--- src/router/index.js
+++ src/router/index.js
@@ -1,13 +1,15 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
 import Login from "../components/Login";
+import Home from "../components/Home";
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
   routes: [
-    { path: "/", redirect: '/login' },
-    { path: '/login', component: Login }
+    {path: "/", redirect: '/login'},
+    {path: '/login', component: Login},
+    {path: '/home', component: Home}
   ]
 })
 
