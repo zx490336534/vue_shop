25f194e9f2ae99be5c0de392ab914a56908cd854
钟鑫 <490336534@qq.com>
2021-01-08 21:16:09 +0800

    渲染Login组件 关闭eslint

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -1,15 +1,15 @@
 <template>
-  <div id="app">
-    <router-view></router-view>
+  <div>
+    登录组件
   </div>
 </template>
 
 <script>
   export default {
-    name: 'app'
+    name: "Login"
   }
 </script>
 
-<style>
+<style lang="less" scoped>
 
 </style>
--- src/router/index.js
+++ src/router/index.js
@@ -1,10 +1,14 @@
 import Vue from 'vue'
 import VueRouter from 'vue-router'
+import Login from "../components/Login";
 
 Vue.use(VueRouter)
 
 const router = new VueRouter({
-  routes: []
+  routes: [
+    { path: "/", redirect: '/login' },
+    { path: '/login', component: Login }
+  ]
 })
 
 export default router
