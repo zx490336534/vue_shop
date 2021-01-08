c4385c45322b6d5dc3a79fa4bb27f8aa999876ee
钟鑫 <490336534@qq.com>
2021-01-09 07:54:31 +0800

    导入消息提示，添加到Vue.prototype

--- src/components/Login.vue
+++ src/components/Login.vue
@@ -67,8 +67,8 @@
         this.$refs.loginFormRef.validate(async valid => {
           if (!valid) return;
           const {data: res} = await this.$http.post("login", this.loginForm)
-          if (res.meta.status !== 200) return console.log('登录失败')
-          console.log('登录成功')
+          if (res.meta.status !== 200) return this.$message.error('登录失败')
+          this.$message.success('登录成功')
         })
       }
     }
