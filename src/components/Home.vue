2d9cfbe01944326b9717aab8c0ce3f7c4fd592f2
钟鑫 <490336534@qq.com>
2021-01-09 08:35:56 +0800

    主页头部布局

--- src/components/Home.vue
+++ src/components/Home.vue
@@ -1,7 +1,11 @@
 <template>
   <el-container class="home-container">
     <!-- 头部区域 -->
-    <el-header>Header
+    <el-header>
+      <div>
+        <img src="../assets/heima.png" alt="">
+        <span>电商后台管理</span>
+      </div>
       <el-button type="info" @click="logout">退出</el-button>
     </el-header>
     <!-- 页面主体区域 -->
@@ -29,6 +33,22 @@
 <style lang="less" scoped>
   .el-header {
     background-color: #373d41;
+    display: flex;
+    justify-content: space-between;
+    padding-left: 0;
+    align-items: center;
+    color: #fff;
+    font-size: 20px;
+
+    > div {
+      display: flex;
+      align-items: center;
+
+      span {
+        margin-left: 15px;
+      }
+    ;
+    }
   }
 
   .el-aside {
