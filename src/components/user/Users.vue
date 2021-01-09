937c35522762da2ebb76e92451bfab00a6f5ce5d
钟鑫 <490336534@qq.com>
2021-01-09 13:42:19 +0800

    用户列表 删除用户

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -280,7 +280,12 @@
         if (confirmResult !== 'confirm') {
           return this.$message.info('已取消删除')
         }
-        this.$message.success('删除成功')
+        const { data: res } = await this.$http.delete('users/' + id)
+        if (res.meta.status !== 200) {
+          return this.$message.error('删除用户失败！')
+        }
+        this.getUserList()
+        this.$message.success('删除用户成功')
       }
     }
   }
