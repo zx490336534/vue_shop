84d36b3669050103c9ee3f0f51a17ca848abfc74
钟鑫 <490336534@qq.com>
2021-01-09 08:05:29 +0800

    挂载路由导航守卫

--- src/router/index.js
+++ src/router/index.js
@@ -12,5 +12,15 @@ const router = new VueRouter({
     {path: '/home', component: Home}
   ]
 })
-
+// 挂载路由导航守卫
+router.beforeEach((to, from, next) => {
+  // to 将要访问的路径
+  // form 从哪个路径跳转而来
+  // next 是一个函数，表示放行
+  if (to.path === '/login') return next();
+  // 获取token
+  const tokenStr = window.sessionStorage.getItem('token')
+  if (!tokenStr) return next('/login')
+  next()
+})
 export default router
