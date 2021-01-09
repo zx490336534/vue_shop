5428509f526ac859e3988fe240d853f6b4fbf33d
钟鑫 <490336534@qq.com>
2021-01-09 11:40:26 +0800

    用户列表 用户添加

--- src/components/user/Users.vue
+++ src/components/user/Users.vue
@@ -62,14 +62,14 @@
         :total="total">
       </el-pagination>
       <!--添加用户对话框-->
-      <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%">
+      <el-dialog title="添加用户" :visible.sync="addDialogVisible" width="50%" @close="addDialogClosed">
         <!-- 内容主体区域-->
         <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="70px">
           <el-form-item label="用户名" prop="username">
             <el-input v-model="addForm.username"></el-input>
           </el-form-item>
           <el-form-item label="密码" prop="password">
-            <el-input v-model="addForm.username"></el-input>
+            <el-input v-model="addForm.password"></el-input>
           </el-form-item>
           <el-form-item label="邮箱" prop="email">
             <el-input v-model="addForm.email"></el-input>
@@ -81,7 +81,7 @@
         <!-- 底部区域-->
         <span slot="footer" class="dialog-footer">
           <el-button @click="addDialogVisible = false">取 消</el-button>
-          <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+          <el-button type="primary" @click="addUser">确 定</el-button>
         </span>
       </el-dialog>
     </el-card>
@@ -180,6 +180,26 @@
           return this.$message.error('更新用户状态失败')
         }
         this.$message.success('更新用户状态成功')
+      },
+      // 监听添加用户对话框的关闭事件
+      addDialogClosed() {
+        this.$refs.addFormRef.resetFields()
+      },
+      //点击按钮，添加新用户
+      addUser() {
+        this.$refs.addFormRef.validate(async valid => {
+          if (!valid) return
+          // 可以发起添加用户的网络请求
+          const { data: res } = await this.$http.post('users', this.addForm)
+          if (res.meta.status !== 201) {
+            this.$message.error('添加用户失败！')
+          }
+          this.$message.success('添加用户成功！')
+          // 隐藏添加用户对话框
+          this.addDialogVisible = false
+          // 重新获取用户列表数据
+          this.getUserList()
+        })
       }
     }
   }
