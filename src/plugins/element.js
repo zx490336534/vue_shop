91d1686b92b40fd8bd628b7abff453e38c54feef
钟鑫 <490336534@qq.com>
2021-01-09 11:19:34 +0800

    用户列表 添加用户表单的渲染与校验

--- src/plugins/element.js
+++ src/plugins/element.js
@@ -21,7 +21,8 @@ import {
   TableColumn,
   Switch,
   Tooltip,
-  Pagination
+  Pagination,
+  Dialog
 } from 'element-ui'
 
 Vue.use(Button)
@@ -45,4 +46,5 @@ Vue.use(TableColumn)
 Vue.use(Switch)
 Vue.use(Tooltip)
 Vue.use(Pagination)
+Vue.use(Dialog)
 Vue.prototype.$message = Message
