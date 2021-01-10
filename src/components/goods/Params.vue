4710e8b026b4a6f4cf57a2bb0533d6b21ad0ce08
钟鑫 <490336534@qq.com>
2021-01-10 09:19:15 +0800

    商品管理-分类参数 注意项与级联分类下拉框选择

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -1,13 +1,80 @@
 <template>
-    <div></div>
+  <div>
+    <!-- 面包屑导航区 -->
+    <el-breadcrumb separator-class="el-icon-arrow-right">
+      <el-breadcrumb-item :to="{ path: '/' }">首页</el-breadcrumb-item>
+      <el-breadcrumb-item>商品管理</el-breadcrumb-item>
+      <el-breadcrumb-item>参数列表</el-breadcrumb-item>
+    </el-breadcrumb>
+    <!--卡片视图区域-->
+    <el-card>
+      <!--警告区-->
+      <el-alert
+        show-icon
+        :closable="false"
+        title="注意：只允许为第三级分类设置相关参数！"
+        type="warning">
+      </el-alert>
+      <!--选择商品分类区域-->
+      <el-row class="cat_opt">
+        <el-col>
+          <span>选择商品分类：</span>
+          <!--选择商品分类的级联选择框-->
+          <el-cascader
+            v-model="selectedCateKeys"
+            :options="catelist"
+            :props="cateProps"
+            @change="handleChange"></el-cascader>
+        </el-col>
+      </el-row>
+
+    </el-card>
+  </div>
 </template>
 
 <script>
   export default {
-    name: 'Params'
+    name: 'Params',
+    data() {
+      return {
+        //商品分类列表
+        catelist: [],
+        //级联选择框对象
+        cateProps: {
+          value: 'cat_id',
+          label: 'cat_name',
+          children: 'children'
+        },
+        //级联选择框双向绑定到的数组
+        selectedCateKeys: []
+      }
+    },
+    created() {
+      this.getCateList()
+    },
+    methods: {
+      //获取所有商品分类列表
+      async getCateList() {
+        const { data: res } = await this.$http.get('categories')
+        if (res.meta.status !== 200) {
+          return this.$message.error('获取商品分类列表失败！')
+        }
+        this.catelist = res.data
+
+      },
+      // 级联选择框选中值变化，会触发这个函数
+      handleChange() {
+        if (this.selectedCateKeys.length !== 3) {
+          this.selectedCateKeys = []
+        }
+      }
+    }
+
   }
 </script>
 
 <style lang="less" scoped>
-
+  .cat_opt {
+    margin: 15px 0;
+  }
 </style>
--- src/plugins/element.js
+++ src/plugins/element.js
@@ -28,7 +28,8 @@ import {
   Tree,
   Select,
   Option,
-  Cascader
+  Cascader,
+  Alert
 } from 'element-ui'
 
 Vue.use(Button)
@@ -58,5 +59,6 @@ Vue.use(Tree)
 Vue.use(Select)
 Vue.use(Option)
 Vue.use(Cascader)
+Vue.use(Alert)
 Vue.prototype.$message = Message
 Vue.prototype.$confirm = MessageBox.confirm
