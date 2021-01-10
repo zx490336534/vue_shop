4b1d3236178127a4201bb701a05822a3de4dacd8
钟鑫 <490336534@qq.com>
2021-01-10 19:30:35 +0800

    商品管理-商品列表 tabs渲染

--- src/components/goods/Add.vue
+++ src/components/goods/Add.vue
@@ -25,14 +25,41 @@
         <el-step title="完成"></el-step>
       </el-steps>
 
-      <!--tab栏区域-->
-      <el-tabs :tab-position="'left'" style="height: 200px;" v-model="activeIndex">
-        <el-tab-pane label="基本信息" name="0">基本信息</el-tab-pane>
-        <el-tab-pane label="商品参数" name="1">商品参数</el-tab-pane>
-        <el-tab-pane label="商品属性" name="2">商品属性</el-tab-pane>
-        <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
-        <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
-      </el-tabs>
+      <el-form ref="addFormRef" :model="addForm" :rules="addFormRules" label-width="100px" label-position="top">
+        <!--tab栏区域-->
+        <el-tabs :tab-position="'left'" v-model="activeIndex" :before-leave="beforeTabLeave" tab-click="tabClicked">
+          <el-tab-pane label="基本信息" name="0">
+            <el-form-item label="商品名称" prop="goods_name">
+              <el-input v-model="addForm.goods_name"></el-input>
+            </el-form-item>
+            <el-form-item label="商品价格" prop="goods_price">
+              <el-input v-model="addForm.goods_price" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品重量" prop="goods_weight">
+              <el-input v-model="addForm.goods_weight" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品数量" prop="goods_number">
+              <el-input v-model="addForm.goods_number" type="number"></el-input>
+            </el-form-item>
+            <el-form-item label="商品分类" prop="goods_cat">
+              <el-cascader
+                expand-trigger="hover"
+                :options="catelist"
+                :props="cateProps"
+                v-model="addForm.goods_cat"
+                @change="handleChange">
+              </el-cascader>
+            </el-form-item>
+
+          </el-tab-pane>
+          <el-tab-pane label="商品参数" name="1">
+          </el-tab-pane>
+          <el-tab-pane label="商品属性" name="2">
+          </el-tab-pane>
+          <el-tab-pane label="商品图片" name="3">商品图片</el-tab-pane>
+          <el-tab-pane label="商品内容" name="4">商品内容</el-tab-pane>
+        </el-tabs>
+      </el-form>
 
     </el-card>
 
@@ -44,12 +71,87 @@
     name: 'Add',
     data() {
       return {
-        activeIndex: '0'
+        activeIndex: '0',
+        //添加商品的表单数据对象
+        addForm: {
+          goods_name: '',
+          goods_price: 0,
+          goods_weight: 0,
+          goods_number: 0,
+          // 商品所属的分类数组
+          goods_cat: [],
+          // 图片的数组
+          pics: [],
+          // 商品的详情描述
+          goods_introduce: '',
+          attrs: []
+        },
+        addFormRules: {
+          goods_name: [{ required: true, message: '请输入商品名称', trigger: 'blur' }],
+          goods_price: [{ required: true, message: '请输入商品价格', trigger: 'blur' }],
+          goods_weight: [{ required: true, message: '请输入商品重量', trigger: 'blur' }],
+          goods_number: [{ required: true, message: '请输入商品数量', trigger: 'blur' }],
+          goods_cat: [
+            { required: true, message: '请选择商品分类', trigger: 'blur' }
+          ]
+        },
+        // 商品分类列表
+        catelist: [],
+        cateProps: {
+          label: 'cat_name',
+          value: 'cat_id',
+          children: 'children'
+        },
+        manyTableData: []
       }
     },
     created() {
+      this.getCateList()
     },
-    methods: {}
+    methods: {
+      // 获取所有商品分类数据
+      async getCateList() {
+        const { data: res } = await this.$http.get('categories')
+        if (res.meta.status !== 200) {
+          return this.$message.error('获取商品分类数据失败！')
+        }
+        this.catelist = res.data
+        console.log(this.catelist)
+      },
+
+      handleChange() {
+        console.log(this.addForm.good_cat)
+        if (this.addForm.goods_cat.length !== 3) {
+          this.addForm.goods_cat.length = []
+        }
+      },
+      beforeTabLeave(activeName, oldActiveName) {
+        console.log('即将离开' + oldActiveName)
+        console.log('即将进入' + activeName)
+        if (oldActiveName === '0' && this.addForm.goods_cat.length !== 3) {
+          this.$message.error('请先选中商品分类！')
+          return false
+        }
+
+      },
+      async tabClicked() {
+        if (this.activeIndex === '1') {
+          const { data: res } = await this.$http.get(`categories/${this.cateId}/attributes`, { params: { sel: 'many' } })
+          if (res.meta.data.status !== 200) {
+            return this.$message.error('获取动态参数列表失败！')
+          }
+          this.manyTableData = res.data
+        }
+      }
+    },
+    computed: {
+      cateId() {
+        if (this.addForm.goods_cat.length === 3) {
+          return this.addForm.goods_cat[2]
+        }
+        return null
+      }
+    }
   }
 </script>
 
--- src/main.js
+++ src/main.js
@@ -10,7 +10,7 @@ import TreeTable from 'vue-table-with-tree-grid'
 
 import axios from 'axios'
 // 配置请求的根路径
-axios.defaults.baseURL = 'https://www.liulongbin.top:8888/api/private/v1/'
+axios.defaults.baseURL = 'http://timemeetyou.com:8889/api/private/v1/'
 // 配置请求拦截器，为每一次请求添加Authorization
 axios.interceptors.request.use(config => {
   config.headers.Authorization = window.sessionStorage.getItem('token')
