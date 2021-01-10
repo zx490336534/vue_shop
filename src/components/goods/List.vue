9e0c3a0ea830ffdef7df85be803ff94e10511061
钟鑫 <490336534@qq.com>
2021-01-10 12:47:48 +0800

    商品管理-商品列表 分页功能与删除商品

--- src/components/goods/List.vue
+++ src/components/goods/List.vue
@@ -10,8 +10,8 @@
     <el-card>
       <el-row :gutter="20">
         <el-col :span="8">
-          <el-input placeholder="请输入内容">
-            <el-button slot="append" icon="el-icon-search"></el-button>
+          <el-input placeholder="请输入内容" v-model="queryInfo.query" clearable @clear="getGoodsList">
+            <el-button slot="append" icon="el-icon-search" @click="getGoodsList"></el-button>
           </el-input>
         </el-col>
         <el-col :span="4">
@@ -32,10 +32,21 @@
         <el-table-column label="操作">
           <template slot-scope="scope">
             <el-button type="primary" icon="el-icon-edit" size="mini"></el-button>
-            <el-button type="danger" icon="el-icon-delete" size="mini"></el-button>
+            <el-button type="danger" icon="el-icon-delete" size="mini"
+                       @click="removeById(scope.row.goods_id)"></el-button>
           </template>
         </el-table-column>
       </el-table>
+      <!--分页区域-->
+      <el-pagination
+        @size-change="handleSizeChange"
+        @current-change="handleCurrentChange"
+        :current-page="queryInfo.pagenum"
+        :page-sizes="[5, 10, 15, 20]"
+        :page-size="queryInfo.pagesize"
+        layout="total, sizes, prev, pager, next, jumper"
+        :total="total" background>
+      </el-pagination>
     </el-card>
   </div>
 </template>
@@ -70,6 +81,30 @@
         this.$message.success('获取商品列表成功')
         this.goodslist = res.data.goods
         this.total = res.data.total
+      },
+      handleSizeChange(newSize) {
+        this.queryInfo.pagesize = newsize
+        this.getGoodsList()
+      },
+      handleCurrentChange(newPage) {
+        this.queryInfo.pagenum = newPage
+        this.getGoodsList()
+      },
+      async removeById(id) {
+        const confirmResult = await this.$confirm('此操作将永久删除该商品, 是否继续?', '提示', {
+          confirmButtonText: '确定',
+          cancelButtonText: '取消',
+          type: 'warning'
+        }).catch(err => err)
+        if (confirmResult !== 'confirm') {
+          return this.$message.info('已取消删除')
+        }
+        const { data: res } = await this.$http.delete('goods/' + id)
+        if (res.meta.status !== 200) {
+          return this.$message.error('删除商品失败！')
+        }
+        this.getGoodsList()
+        this.$message.success('删除商品成功')
       }
     }
   }
