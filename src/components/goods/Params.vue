22d34a9e386bde2768ef7c16ba50ea5cdada7a76
钟鑫 <490336534@qq.com>
2021-01-10 10:07:05 +0800

    商品管理-分类参数 添加参数按钮对话框

--- src/components/goods/Params.vue
+++ src/components/goods/Params.vue
@@ -31,14 +31,56 @@
       <el-tabs v-model="activeName" @tab-click="handleTabClick">
         <!--添加动态参数的面板-->
         <el-tab-pane label="动态参数" name="many">
-          <el-button type="primary" size="mini" :disabled="isBtnDiabled">添加参数</el-button>
+          <el-button type="primary" size="mini" :disabled="isBtnDiabled" @click="addDialogVisible=true">添加参数</el-button>
+          <el-table :data="manyTableData" border stripe>
+            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="参数名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+                <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
         <!--添加静态属性的面板-->
         <el-tab-pane label="静态属性" name="only">
-          <el-button type="primary" size="mini" :disabled="isBtnDiabled">添加属性</el-button>
+          <el-button type="primary" size="mini" :disabled="isBtnDiabled" @click="addDialogVisible=true">添加属性</el-button>
+          <el-table :data="onlyTableData" border stripe>
+            <el-table-column type="expand"></el-table-column>
+            <el-table-column type="index"></el-table-column>
+            <el-table-column label="属性名称" prop="attr_name"></el-table-column>
+            <el-table-column label="操作">
+              <template slot-scope="scope">
+                <el-button type="primary" icon="el-icon-edit" size="mini">编辑</el-button>
+                <el-button type="danger" icon="el-icon-delete" size="mini">删除</el-button>
+              </template>
+            </el-table-column>
+          </el-table>
         </el-tab-pane>
       </el-tabs>
     </el-card>
+
+    <!--      添加参数的对话框-->
+    <el-dialog
+      :title="'添加'+titleText+':'"
+      :visible.sync="addDialogVisible"
+      width="50%"
+      @close="addDialogClosed"
+    >
+      <el-form :model="addForm" :rules="addFormRules" ref="addFormRef" label-width="100px">
+        <el-form-item :label="titleText" prop="attr_name">
+          <el-input v-model="addForm.attr_name"></el-input>
+        </el-form-item>
+      </el-form>
+
+      <span slot="footer" class="dialog-footer">
+        <el-button @click="addDialogVisible = false">取 消</el-button>
+        <el-button type="primary" @click="addDialogVisible = false">确 定</el-button>
+      </span>
+    </el-dialog>
+
   </div>
 </template>
 
@@ -62,7 +104,19 @@
         //动态参数数据
         manyTableData: {},
         //静态属性数据
-        onlyTableData: {}
+        onlyTableData: {},
+        //控制添加对话框的显示与隐藏
+        addDialogVisible: false,
+        //添加参数的表单数据对象
+        addForm: {
+          attr_name: ''
+        },
+        //添加表单的验证规则对象
+        addFormRules: {
+          attr_name: [
+            { required: true, message: '请输入参数名称', trigger: 'blur' }
+          ]
+        }
       }
     },
     computed: {
@@ -79,6 +133,15 @@
           return this.selectedCateKeys[2]
         }
         return null
+      },
+      //动态计算
+      titleText() {
+        if (this.activeName === 'many') {
+          return '动态参数'
+        } else {
+          return '静态属性'
+
+        }
       }
     },
     created() {
@@ -107,19 +170,25 @@
         if (this.selectedCateKeys.length !== 3) {
           this.selectedCateKeys = []
         }
-        //根据所选分类的ID，和当前所处面板获取相应参数
-        const { data: res } = await this.$http.get(`categories/${this.cateId}/attributes`,
-          {
-            params: { sel: this.activeName }
-          })
-        if (res.meta.status !== 200) {
-          return this.$message.error('获取参数列表失败！')
+        if (this.cateId !== null) {
+          //根据所选分类的ID，和当前所处面板获取相应参数
+          const { data: res } = await this.$http.get(`categories/${this.cateId}/attributes`,
+            {
+              params: { sel: this.activeName }
+            })
+          if (res.meta.status !== 200) {
+            return this.$message.error('获取参数列表失败！')
+          }
+          if (this.activeName === 'many') {
+            this.manyTableData = res.data
+          } else {
+            this.onlyTableData = res.data
+          }
         }
-        if (this.activeName === 'many') {
-          this.manyTableData = res.data
-        } else {
-          this.onlyTableData = res.data
-        }
+      },
+      // 监听添加对话框的关闭事件
+      addDialogClosed() {
+        this.$refs.addFormRef.resetFields()
       }
     }
 
